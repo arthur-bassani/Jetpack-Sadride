4d31d538368d084f4e9a3349b0d59656bb8ee4e0
eduardin gamer <61622527+NOOBPRO4EVER@users.noreply.github.com>
2024-12-23 13:03:45 -0300

    sadride.h

--- sadride.h
+++ sadride.h
@@ -1,6 +1,15 @@
-int tela_inicial();
+typedef enum {  // Estados do Jogo
+    MENU,
+    JOGO,
+    LEADERBOARD,
+    SAIR
+}EstadoJogo;
+
+EstadoJogo tela_inicial();
 void movimento(int *y, float *velocidade, int jetpackaltura);
 
 //constantes da janela
 #define JANELA_Y 450
 #define JANELA_X 800
+#define BOTAO_LARG 200
+#define BOTAO_ALT 50
